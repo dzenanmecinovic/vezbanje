4dcd30335ca328ee6721d646786835547c20d2db
dzenanmecinovic <mecinovicd@gmail.com>
2023-11-02 00:24:43 +0100

    strings

--- main.c
+++ main.c
@@ -1,9 +1,18 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
-    char ime[30]; 
-    printf("\nUnesite ime:   ");
-    scanf("%s", &ime);
-    printf("\n\nPozdrav, %s.", ime);
+    char ime[25]; // bajtovi
+    int godine;
+
+    printf("\nUnesite ime: ");
+    fgets(ime, 25, stdin);
+    ime[strlen(ime)-1] = '\0';
+
+    printf("\nKoliko godina imate? ");
+    scanf("%d", &godine);
+
+    printf("\n\nPozdrav %s, hahahaha.", ime);
+    printf("\nImas %d godina.", godine);
     return 0;
     }
