6d0fc13e06f0509b89ace0eb833fa5a7338045de
dzenanmecinovic <mecinovicd@gmail.com>
2023-11-02 00:34:10 +0100

    math funcs

---
+++ mathfuncs.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+#include <math.h>
+
+int main() {
+
+    double A = sqrt(9); // 3 kvadratni koren broja
+    double B = pow(2, 4); // 16 stepenovanje
+    int C = round(3.14); // 3 zaokruzivanje na ceo broj
+    int D = ceil(3.14); // 4 zaokruzivanje na veci broj
+    int E = floor(3.99); // 3 zaokruzivanje na manji broj
+    double F = fabs(-100); // 100 apsolutna vrednost broja
+    double G = log(3); // logaritam
+    double H = sin(45); // sinus
+    double I = cos(45); // kosinus
+    double J = tan(45);  // tangens
+
+    printf("\n%lf", I);
+
+    return 0;
+}
