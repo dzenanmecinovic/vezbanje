865194c8e0b9a5b9cf97dc6588f33ced2986ab76
Dženan Mecinović <mecinovicdz@gmail.com>
2024-01-04 23:21:11 +0100

    nizovi

--- kol/nizovi.c
+++ kol/nizovi.c
@@ -10,6 +10,83 @@
 int main()
 {
 
+    // OPERACIJE SA NIZOVIMA I RAZVRSTAVANJE ELEMENATA
+
+    // 10.9. Саставити програм који ће за унети низ (дужине n) целих бројева одредити и исписати:
+    // а) суму парних и суму непарних бројева;
+    // б) суму елемената са парним индексима и суму елемената са непарним индексима
+    // int i = 0, n, niz[10], sumaParnih = 0, sumaNeparnih = 0, sumaElParniIndeks = 0, sumaElNeparniIndeks = 0;
+    // printf("Broj elemenata: ");
+    // scanf("%d", &n);
+    // printf("Elementi niza: ");
+    // for (i; i < n; i++)
+    // {
+    //     scanf("%d", &niz[i]);
+    //     if (niz[i] % 2 == 0)
+    //     {
+    //         sumaParnih += niz[i];
+    //     }
+    //     else
+    //     {
+    //         sumaNeparnih += niz[i];
+    //     }
+    //     if (i % 2 == 0)
+    //     {
+    //         sumaElParniIndeks += niz[i];
+    //     }
+    //     else
+    //     {
+    //         sumaElNeparniIndeks += niz[i];
+    //     }
+    // }
+
+    // printf("Suma parnih elemenata: %d\n", sumaParnih);
+    // printf("Suma neparnih elemenata: %d\n", sumaNeparnih);
+    // printf("Suma el sa parnim indeksom: %d\n", sumaElParniIndeks);
+    // printf("Suma el sa neparnim indeksom: %d\n", sumaElNeparniIndeks);
+
+    // 10.8. Саставити програм који за унети низ (дужине n) целих бројева израчунава и исписуje аритметичку средину оних елемената низа који су дељиви са 3.
+    // int i, n, niz[10], counter = 0, suma = 0;
+    // printf("Broj elemenata: ");
+    // scanf("%d", &n);
+    // printf("Elementi niza: ");
+    // for (i = 0; i < n; i++)
+    // {
+    //     scanf("%d", &niz[i]);
+    //     if (niz[i] % 3 == 0)
+    //     {
+    //         suma += niz[i];
+    //         counter++;
+    //     }
+    // }
+    // printf("aritmeticka: %.2f\n", (float)suma / counter);
+
+    // 10.7. Саставити програм за израчунавање и исписивање аритметичке средине задатог низа (дужине n) целих бројева.
+    // int i, n, niz[10], suma = 0;
+    // printf(" Broj elemenata: ");
+    // scanf("%d", &n);
+    // printf(" Elementi niza: ");
+    // for (i = 0; i < n; i++)
+    // {
+    //     scanf("%d", &niz[i]);
+    //     suma += niz[i];
+    // }
+    // printf(" aritmeticka = %.0f\n", (float)suma / n);
+
+    // 10.6. a)
+
+    // int niz[10];
+    // printf("\n Niz zauzima %d bajtova.", sizeof(niz) / sizeof(int));
+
+    // 10.6. b)
+
+    // int i, brojElemenata;
+    // int niz[] = {1, 2, 3, 4, 5, 6};
+    // brojElemenata = sizeof(niz) / sizeof(int);
+    // for (i = 0; i < brojElemenata; i++)
+    // {
+    //     printf("niz[%d]=%d\n", i, niz[i]);
+    // }
     // 10.5. Саставити програм који ће учитати низ од n елемената и исписати их обрнутим редоследом
 
     // int i = 0, n, niz[20];
