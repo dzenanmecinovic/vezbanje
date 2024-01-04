79e2bbe93b6436c360a599b0b37cb52261a398bd
Dženan Mecinović <mecinovicdz@gmail.com>
2024-01-04 22:42:34 +0100

    nizovi

--- kol/nizovi.c
+++ kol/nizovi.c
@@ -1,18 +1,88 @@
 #include <stdio.h>
+// #define MAX 10
 
 // primer
-void f(int a[], int size)
-{
-    printf("f: %d\n", size);
-}
+// void f(int a[], int size)
+// {
+//     printf("f: %d\n", size);
+// }
 
 int main()
 {
 
-    int d[] = {1, 2, 3};
-    printf("main %d\n", sizeof(d) / sizeof(d[0]));
-    f(d, sizeof(d) / sizeof(d[0]));
-    return 0;
+    // 10.5. Саставити програм који ће учитати низ од n елемената и исписати их обрнутим редоследом
+
+    // int i = 0, n, niz[20];
+
+    // printf("n:");
+    // scanf("%d", &n);
+    // printf("Unesite niz od %d elemenata: ", n);
+    // for (i; i < n; i++)
+    // {
+    //     scanf("%d", &niz[i]);
+    // }
+
+    // printf("Obrnutni niz: ");
+
+    // for (i = n - 1; i >= 0; i--)
+    // {
+    //     printf("%d ", niz[i]);
+    // }
+
+    // 10.4. Саставити програм који ће учитати низ од n елемената и исписати их оним редоследом којим су учитани.
+
+    // int i, n, niz[20];
+    // printf("n:");
+    // scanf("%d", &n);
+    // printf("Unesite %d elemenata: ", n);
+    // for (i = 0; i < n; i++)
+    // {
+    //     scanf("%d", &niz[i]);
+    // }
+    // printf("\nUneti niz: ");
+    // for (i = 0; i < n; i++)
+    // {
+    //     printf(" %d", niz[i]);
+    // }
+    // int i, j, niz[] = {15, 3, 9, 7, 11};
+    // printf("\nRedni broj\tIndeks\tVrednost\n");
+    // for (i = 0; i < 5; i++)
+    // {
+    //     printf("\n%d\t\t%d\t%d", i + 1, i, niz[i]);
+    // }
+    // int i, n, niz[20];
+    // printf(" n= ");
+    // scanf("%d", &n);
+    // for (i = 0; i < n; i++)
+    // {
+    //     printf("%d. element niza: ", i + 1);
+    //     scanf("%d", &niz[i]);
+    // }
+    // for (i = 0; i < n; i++)
+    // {
+    //     printf("\n%d. element niza: %d", i + 1, niz[i]);
+    // }
+    // int someNumbers[] = {5, 25, 100, 500};
+    // char someChars[] = {'A', 'B', 'C', 'D'};
+
+    // int x = 0;
+
+    // for (x; x < 4; x++)
+    // {
+    //     printf("Number => %d \n", someNumbers[x]);
+    // }
+
+    // printf("number: %d \n", someNumbers[0]);
+    // printf("number: %d \n", someNumbers[1]);
+    // printf("number: %d \n", someNumbers[3]);
+
+    // someChars[0] = 'K';
+    // printf("Char: %c \n", someChars[0]);
+
+    // int d[] = {1, 2, 3};
+    // printf("main %d\n", sizeof(d) / sizeof(d[0]));
+    // f(d, sizeof(d) / sizeof(d[0]));
+    // return 0;
 
     /*
         Funkcija koja prima niz moze bit deklarisana na neki od sledecih nacina:
@@ -23,4 +93,6 @@ int main()
         Posto se u funkciju prenosi samo adresa pocetka niza, a ne i dimenzija niza,
         prvi oblik deklaracije i nema puno smisla.
     */
+
+    return 0;
 }
