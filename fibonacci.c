d8f1d98474174a7dfce2c257a155c11cb843d2f3
Dženan Mecinović <mecinovicdz@gmail.com>
2024-02-19 19:21:42 +0100

    fibonacci

---
+++ fibonacci.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+
+int main()
+{
+    int i, n, next, first = 0, second = 1;
+
+    printf("Unesite broj do kojeg zelite Fibonaccijev niz:");
+    scanf("%d", &n);
+
+    for (i = 0; i < n; i++)
+    {
+        next = first + second;
+        first = second;
+        second = next;
+
+        printf("%d ", next);
+    }
+
+    return 0;
+}
+
+// PRVI je DRUGI, a DRUGI je DOBIJENI
